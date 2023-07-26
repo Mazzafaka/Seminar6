2c9730eb80353e1cd3597de756ecb9e5f53eda3d
Zack <1984odina.gmail.com>
2023-07-26 21:52:49 +0600

    Added all

---
+++ Exs/Program.cs
@@ -0,0 +1,24 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+
+int FindNumbers(int count){
+    int positiveCount = 0;
+    count = 4;
+
+            for (int i = 0; i < count; i++)
+            {
+                Console.Write($"Введите число {i + 1}: ");
+                int number = int.Parse(Console.ReadLine());
+
+                if (number > 0)
+                {
+                    positiveCount++;
+                }
+            }
+
+            Console.WriteLine($"Количество чисел, больше 0: {positiveCount}");
+    return positiveCount;
+}   
+
+int Print = 4;
+FindNumbers(Print);
---
+++ Exs2/Program.cs
@@ -0,0 +1,39 @@
+﻿
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите значение k1: ");
+            double k1;
+            while (!double.TryParse(Console.ReadLine(), out k1))
+            {
+                Console.WriteLine("Некорректный ввод. Попробуйте снова.");
+            }
+
+            Console.Write("Введите значение b1: ");
+            double b1;
+            while (!double.TryParse(Console.ReadLine(), out b1))
+            {
+                Console.WriteLine("Некорректный ввод. Попробуйте снова.");
+            }
+
+            Console.Write("Введите значение k2: ");
+            double k2;
+            while (!double.TryParse(Console.ReadLine(), out k2))
+            {
+                Console.WriteLine("Некорректный ввод. Попробуйте снова.");
+            }
+
+            Console.Write("Введите значение b2: ");
+            double b2;
+            while (!double.TryParse(Console.ReadLine(), out b2))
+            {
+                Console.WriteLine("Некорректный ввод. Попробуйте снова.");
+            }
+
+            double x = (b2 - b1) / (k1 - k2);
+            double y = k1 * x + b1;
+
+            Console.WriteLine($"Точка пересечения прямых: ({x}, {y})");
+        }
+    }
